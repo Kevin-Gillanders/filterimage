488a970ee3e63aae62726e8b13f0979d3913c57e
Kevin-Gillanders <kevingillanders1@gmail.com>
2018-04-03 16:54:13 +0100

    Update window.py

--- window.py
+++ window.py
@@ -16,31 +16,34 @@ def slidingWindow(image, windowSize):
 		image[idx] = tmp
 	tmp = []
 	size = len(image)
-	print()
+	# pp.pprint(image)
 	fin = []
 	# merges chunks along the y axis 
-	for row in range(0, size):
-		tmp.append(image[row])
-		if (row + 1) % windowSize == 0:
-			print(list(zip(*tmp)))
-			fin.append(tmp)	
-			tmp = []
+	for x in range(0, len(image), windowSize):
+		# tmp.append(image[row])
+		fin.append(zip(*image[x:x + windowSize]))
+		# if (row + 1) % windowSize == 0:
+			# print(list(zip(*tmp)))
+			# fin.append(tmp)	
+			# tmp = []
 		# print(image[row])
-
+	# for x in fin:
+		# for y in x : 
+			# pp.pprint(y)
 	# pp.pprint(zip(*image))
 	return(fin)
 
-win =  [[1,     2,     3,     4],
-		[11,    22,    33,    44],
-		[111,   222,   333,   444],
-		[1111,  2222,  3333,  4444],
-		[11111, 22222, 33333, 44444]]
-# win = []
-# for x in range(0, 15):
-	# tmp = []
-	# for y in range(0, 10):
-		# tmp.append(y)
-	# win.append(tmp)
+# win =  [[1,     2,     3,     4],
+		# [11,    22,    33,    44],
+		# [111,   222,   333,   444],
+		# [1111,  2222,  3333,  4444],
+		# [11111, 22222, 33333, 44444]]
+win = []
+for x in range(0, 1280):
+	tmp = []
+	for y in range(0, 720):
+		tmp.append(y)
+	win.append(tmp)
 	
 print("Amount : {}".format(len(win)* len(win[0])))
 t = time.time()
@@ -49,4 +52,4 @@ win = slidingWindow(win, 3)
 
 print("time took : {} ".format(time.time() - t))
 
-pp.pprint(win)
+# pp.pprint(win)
