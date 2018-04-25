c90435d32c610c0c38162da3dd95cc0b334f9efb
Kevin-Gillanders <kevingillanders1@gmail.com>
2018-04-25 22:24:42 +0100

    test program splits into rgb and filters

--- test.py
+++ test.py
@@ -1,18 +1,45 @@
 from PIL import Image
 import window
-
-image = Image.open("Jelly_Beans.jpg").convert('L')
-
+import time
+image = Image.open("Jelly_Beans.jpg")#.convert('L')
+win = 3
 width, height = image.size
 
 print(width, height)
-px = list(image.getdata())
-print(px)
 
-# for y in range(0, width):
-	# for x in range(0, height):
-		# pix = px[y][x]
-		# print(pix)
+t = time.time()
+px = list(image.getdata())
+
+
+
+red, green, blue = zip(*px)
+red = list(red)
+green = list(green)
+blue = list(blue)
+
+# print(red)
+
+
+# window.slide(px, 3, width, height)
+window.slide(red, win, width, height)
+window.slide(green, win, width, height)
+window.slide(blue, win, width, height)
+
+
+px = zip(red, green, blue)
+px = list(px)
+# print(px[0])
+
+# print(px)
+
+# for y in range(0, len(px)):
+	# print(px[y])
+	# px[y] = (px[y][0], 0, 0)
+	# px[y] = (0, px[y][1], 0)
+	# px[y] = (0, 0, px[y][2])
 
 image.putdata(px)
+image.save(str(win)+'WindowSize.bmp')
 image.show()
+
+print('it took : {}'.format(time.time()-t))
