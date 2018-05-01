0510cb79871e21e0eda1a12b8344177971b913fe
Kevin-Gillanders <kevingillanders1@gmail.com>
2018-05-01 20:39:46 +0100

    Fixed bug, filtering on filtered image

--- test.py
+++ test.py
@@ -1,49 +1,61 @@
 from PIL import Image
 import window
 import time
-image = Image.open("./testImages/frymire.png")#.convert('L')
-win = 3
-minMax = False
-width, height = image.size
-size = min(width, height)
-print(width, height)
+import os
 
-t = time.time()
+print(os.listdir('./testImages'))
 
-for win in range(1, size):
-	px = list(image.getdata())
+for images in os.listdir('./testImages'):
+	image = Image.open("./testImages/" + images)#.convert('L')
+	win = 3
+	minMax = False
+	width, height = image.size
+	size = min(width, height)
+	print(width, height)
 
-	red, green, blue = zip(*px)
-	red = list(red)
-	green = list(green)
-	blue = list(blue)
+	if not (os.path.exists('./max/' + images + '/')):
+		os.makedirs('./max/' + images + '/')
+	elif not(os.path.exists('./min/' + images + '/')):
+		os.makedirs('./min/' + images + '/')
+		
+	t = time.time()
+	for both in range(0, 2):
+		minMax = not minMax
+		for win in range(1, min(100, width, height):
+			image = Image.open("./testImages/" + images)#.convert('L')
+			px = list(image.getdata())
 
-	# print(red)
+			red, green, blue = zip(*px)
+			red = list(red)
+			green = list(green)
+			blue = list(blue)
+
+			# print(red)
 
 
-	# window.slide(px, 3, width, height)
-	window.slide(red, win, width, height, minMax)
-	window.slide(green, win, width, height, minMax)
-	window.slide(blue, win, width, height, minMax)
+			# window.slide(px, 3, width, height)
+			window.slide(red, win, width, height, minMax)
+			window.slide(green, win, width, height, minMax)
+			window.slide(blue, win, width, height, minMax)
 
 
-	px = zip(red, green, blue)
-	px = list(px)
-	# print(px[0])
+			px = zip(red, green, blue)
+			px = list(px)
+			# print(px[0])
 
-	# print(px)
+			# print(px)
 
-	# for y in range(0, len(px)):
-		# print(px[y])
-		# px[y] = (px[y][0], 0, 0)
-		# px[y] = (0, px[y][1], 0)
-		# px[y] = (0, 0, px[y][2])
+			# for y in range(0, len(px)):
+				# print(px[y])
+				# px[y] = (px[y][0], 0, 0)
+				# px[y] = (0, px[y][1], 0)
+				# px[y] = (0, 0, px[y][2])
 
-	image.putdata(px)
-	if minMax:
-		image.save('./max/' + str(win)+'WindowSize.jpeg', 'jpeg')
-	else:
-		image.save('./min/' +str(win)+'WindowSize.jpeg', 'jpeg')
-	# image.show()
+			image.putdata(px)
+			if minMax:
+				image.save('./max/' + images + '/' + str(win)+'WindowSize.jpeg', 'jpeg')
+			else:
+				image.save('./min/' + images + '/' + str(win)+'WindowSize.jpeg', 'jpeg')
+			# image.show()
 
 print('it took : {}'.format(time.time()-t))
